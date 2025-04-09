2ad8e81cfb091d353141f5a675201da9b9b5afb7
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-09 19:00:07 -0300

    extends class abstract in children class

--- 11_abstract_class.dart
+++ 11_abstract_class.dart
@@ -1,6 +1,18 @@
 void main () {
 
-    final windPlan = EnergyPlan();
+    final windPlan = WindPlant(initialEnergy: 1000);
+
+    print ('wind : ${chargePhone (windPlan)}'); //llamo a la funcion chargePhone y le paso el objeto windPlan que es de tipo WindPlant
+
+}
+
+double chargePhone (EnergyPlan plant){ //cualquier plant que extenda de EnergyPlan, por elejempo 
+                                        //WindPlant, se va a poder implementar en esta funcion
+    if(plant.energyLeft < 10){
+      throw Exception('Not enough energy to charge the phone');
+    }
+
+    return plant.energyLeft - 10; //si no hay suficiente energia, lanza una excepcion 
 }
 
 enum PlanType {
@@ -9,6 +21,8 @@ enum PlanType {
     water
 } // LOS ENUM SON LOS UNICOS QUE NO TERMINAN EN ; EN DART
 
+//Este seria la clase Molde o Abstracta EnergyPlan
+//que define las variables y los metodos que van a tener las clases que hereden de ella
 abstract class EnergyPlan {
 
     double energyLeft;
@@ -20,7 +34,20 @@ abstract class EnergyPlan {
         required this.type,
     });
 
-    void consumeEnergy(double amount) ; //metodo abstracto se pueden ir definiendo pero no los implementan o si
+    void consumeEnergy(double amount) ; //metodo abstracto o solo su "firma" se pueden ir definiendo pero no los implementan o si
 }
-
 //Esta seria la definicion de la clase abstracta EnergyPlan
+class WindPlant extends EnergyPlan { //voy a heredar las variables, el constructor 
+//y sus metodos de EnergyPlan
+
+  WindPlant ({required double initialEnergy}
+  ) : super ( //super llama al constructor de la clase padre
+    energyLeft: initialEnergy,
+    type: PlanType.wind,
+  );
+
+  @override //indica que estoy sobreescribiendo el metodo consumeEnergy de la clase EnergyPlan
+  void consumeEnergy(double amount) { //implementacion del metodo abstracto consumeEnergy de la clase EnergyPlan
+    energyLeft -= amount;
+  } //implementacion del metodo abstracto consumeEnergy de la clase EnergyPlan
+}
